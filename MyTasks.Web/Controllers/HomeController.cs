d93b999f5d556c04ea085d6affaa94eba6f4356c
wesleysousukee@gmail.com <wesleysousukee@gmail.com>
2023-03-17 01:09:56 -0300

    Adiciona views e métodos no controller

--- MyTasks.Web/Controllers/HomeController.cs
+++ MyTasks.Web/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using MyTasks.Dto.Dto;
 using MyTasks.Infra.Interface;
 
@@ -8,11 +9,15 @@ namespace Controllers
     {
         //private readonly ILogger<HomeController> _logger;
         private readonly IBaseCrudRepository<MyTasksDto> _MyTasksRepository;
+        private readonly IBaseCrudRepository<ProjectDto> _MyProjectRepository;
+        private readonly ILogErrorRepository _log;
 
-        public HomeController(IBaseCrudRepository<MyTasksDto> MyTasksRepository)
+        public HomeController(IBaseCrudRepository<MyTasksDto> MyTasksRepository, IBaseCrudRepository<ProjectDto> myProjectRepository, ILogErrorRepository log)
         {
             // _logger = logger;
             _MyTasksRepository = MyTasksRepository;
+            _MyProjectRepository = myProjectRepository;
+            _log = log;
         }
 
         public async Task<IActionResult> Index()
@@ -23,16 +28,42 @@ namespace Controllers
         [HttpGet]
         public async Task<IActionResult> CreateNewTask()
         {
+            ViewBag.ProjectId = new SelectList(await _MyProjectRepository.GetAll(), "Id", "ProjectName");
             return View();
         }
 
         [HttpPost]
         public async Task<IActionResult> CreateNewTask(MyTasksDto dto)
         {
-            var createMyTasks = await _MyTasksRepository.Create(dto);
-            return View(createMyTasks);
+            try
+            {
+                var createMyTasks = await _MyTasksRepository.Create(dto);
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                await _log.Create(ex.Message, ex.StackTrace, ex.Source);
+                return View();
+            }
         }
 
+        [HttpGet]
+        public async Task<IActionResult> CreateNewProject()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateNewProject(ProjectDto dto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            var newProect = await _MyProjectRepository.Create(dto);
+            return RedirectToAction("Index");
+
+        }
         public IActionResult ChangeLog()
         {
             return View();

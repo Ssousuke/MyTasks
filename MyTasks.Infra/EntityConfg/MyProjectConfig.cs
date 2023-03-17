0fde428fcaa02371786a8699ab7cbbc7a49eee9a
wesleysousukee@gmail.com <wesleysousukee@gmail.com>
2023-03-17 01:08:32 -0300

    Configura entidade Project

---
+++ MyTasks.Infra/EntityConfg/MyProjectConfig.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MyTasks.Domain.Entities;
+using MyTasks.Domain.Entities.Auxiliar.Enum;
+
+namespace MyTasks.Infra.EntityConfg
+{
+    public class MyProjectConfig : IEntityTypeConfiguration<Project>
+    {
+        public void Configure(EntityTypeBuilder<Project> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.ProjectName).HasMaxLength(75).IsRequired();
+            builder.Property(x => x.ProjectDescription).HasMaxLength(150).IsRequired();
+            builder.Property(x => x.ProjectStep).HasDefaultValue(ProjectStep.AGUARDANDO_MyTasksE);
+        }
+    }
+}

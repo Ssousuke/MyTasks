97a8113fc0151518a96d6e859e23691782031056
wesleysousukee@gmail.com <wesleysousukee@gmail.com>
2023-03-20 20:10:29 -0300

    ajusta entidades

--- MyTasks.Domain/Entities/Auxiliar/Enum/ProjectStep.cs
+++ MyTasks.Domain/Entities/Auxiliar/Enum/ProjectStep.cs
@@ -4,22 +4,13 @@ namespace MyTasks.Domain.Entities.Auxiliar.Enum
 {
     public enum ProjectStep
     {
-        [Description("Aguardando MyTaskse")]
-        AGUARDANDO_MyTasksE = 0,
-
-        [Description("Levantamento de requisitos")]
-        LEVANTAMENTO_REQUISITOS = 1,
+        [Description("Em andamento")]
+        EM_ANDAMENTO = 1,
 
         [Description("Implementação")]
         IMPLEMENTACAO = 2,
 
-        [Description("Testes")]
-        TESTES = 3,
-
-        [Description("Manutenção")]
-        MANUTENCAO = 4,
-
         [Description("Finalizado")]
-        FINALIZADO = 5,
+        FINALIZADO = 3,
     }
 }

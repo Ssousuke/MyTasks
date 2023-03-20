19b7c290be58dc36a63036f46c311ea97ed936ad
wesleysousukee@gmail.com <wesleysousukee@gmail.com>
2023-03-20 20:12:27 -0300

    adiciona validação data annotation

--- MyTasks.Dto/Dto/LogErrorDto.cs
+++ MyTasks.Dto/Dto/LogErrorDto.cs
@@ -5,6 +5,7 @@
         public Guid Id { get; set; }
         public string Message { get; set; }
         public string Source { get; set; }
+        public string StackTrace { get; set; }
         public DateTime CreatedAt { get; set; }
     }
 }
